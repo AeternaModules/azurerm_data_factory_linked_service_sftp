103d13895c1dcaa5935ec98350e7112770f63881
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:45:33 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "data_factory_linked_service_sftps" {
+  description = "All data_factory_linked_service_sftp resources"
+  value       = azurerm_data_factory_linked_service_sftp.data_factory_linked_service_sftps
+}
+
